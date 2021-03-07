35ceec7e5f7d0b66dca639e594da462d0e2c8887
AlexDenisenkova <80042039+AlexDenisenkova@users.noreply.github.com>
2021-03-08 02:24:53 +0300

    Добавлен метод в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,9 +6,12 @@ public class Point {
   public double y;
 
   public Point(double x, double y) {
-    this.x=x;
-    this.y=y;
+    this.x = x;
+    this.y = y;
+  }
 
+  public double distance(Point p2) {
+    return Math.sqrt(((this.x - p2.x) * (this.x - p2.x)) + ((this.y - p2.y) * (this.y - p2.y)));
   }
 
 }
