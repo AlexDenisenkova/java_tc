42376450aaa294df14c924d0bb3559d234d58bd4
AlexDenisenkova <80042039+AlexDenisenkova@users.noreply.github.com>
2021-05-10 14:10:53 +0300

    №11: Реализован тест для проверки информации о контактах на главной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -117,9 +117,11 @@ public class ContactHelper extends HelperBase {
       String firstName = cells.get(2).getText();
       String lastName =  cells.get(1).getText();
       String allPhones = cells.get(5).getText();
+      String address = cells.get(3).getText();
+      String allEmails = cells.get(4).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       contactCache.add(new ContactData().withId(id).withName(firstName).withLastname(lastName)
-              .withAllPhones(allPhones));
+              .withAllPhones(allPhones).withAddress(address).withAllEmails(allEmails));
     }
     return new Contacts(contactCache);
   }
@@ -131,8 +133,14 @@ public class ContactHelper extends HelperBase {
     String home = wd.findElement(By.name("home")).getAttribute("value");
     String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
     String work = wd.findElement(By.name("work")).getAttribute("value");
+    String address = wd.findElement(By.name("address")).getAttribute("value");
+    String email = wd.findElement(By.name("email")).getAttribute("value");
+    String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+    String email3 = wd.findElement(By.name("email3")).getAttribute("value");
     wd.navigate().back();
-    return new ContactData().withId(contact.getId()).withName(firstname).withLastname(lastname).withHomephone(home).withMobile(mobile).withWorkPhone(work);
+    return new ContactData().withId(contact.getId()).withName(firstname).withLastname(lastname)
+            .withHomephone(home).withMobile(mobile).withWorkPhone(work).withAddress(address)
+            .withEmail(email).withEmail2(email2). withEmail3(email3);
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -16,7 +16,9 @@ public class ContactPhoneTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().homePage();
     if (app.contact().all().size() == 0) {
-      app.contact().create(new ContactData().withName("Name").withLastname("Name2").withNickname("Nick").withCompany("Home").withAddress("Lunnaya st. 10").withHomephone("1234456").withMobile("87981236655").withEmail("name@yandex.ru").withGroup("test1"), true);
+      app.contact().create(new ContactData().withName("Name").withLastname("Name2").withNickname("Nick").withCompany("Home")
+              .withAddress("Lunnaya st. 10").withHomephone("1234456").withMobile("87981236655").withEmail("name@yandex.ru")
+              .withGroup("test1").withWorkPhone("123456789").withEmail2("46579879@jkhkjs").withEmail3("kshdfkjsdh@123.gh"), true);
     }
   }
 
@@ -27,6 +29,15 @@ public class ContactPhoneTests extends TestBase {
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
   }
 
   private String mergePhones(ContactData contact) {
@@ -36,7 +47,9 @@ public class ContactPhoneTests extends TestBase {
             .collect(Collectors.joining("\n"));
   }
 
+
   public static String cleaned(String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]","");
   }
+  
 }
